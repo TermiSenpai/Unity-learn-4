f7a3e000e0709ac82c41cce1eaad425b5e3312d2
Termi <escarpaprieto@gmail.com>
2021-08-13 21:16:36 +0200

    Camera can´t rotate without player

--- Assets/Scripts/cameraRotate.cs
+++ Assets/Scripts/cameraRotate.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class cameraRotate : MonoBehaviour
 {
+
+    private playerMovement playerMovementScript;
+
     private float horizontalMove;
     public float rotationSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovementScript = GameObject.Find("player").GetComponent<playerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
-        horizontalMove += rotationSpeed * Input.GetAxis("Mouse X");
-        transform.eulerAngles = new Vector3(0.0f, horizontalMove, 0.0f);
+        if (!playerMovementScript.gameOver)
+        {
+            horizontalMove += rotationSpeed * Input.GetAxis("Mouse X");
+            transform.eulerAngles = new Vector3(0.0f, horizontalMove, 0.0f);
+        }
     }
 }
